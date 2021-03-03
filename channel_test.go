11b37951cc02ae9f0fe510ff4d7a44fcf74a703a
Andy Natalino <andy.natalino@gmail.com>
2021-03-03 23:44:03 +0700

    go routine and channel

---
+++ channel_test.go
@@ -0,0 +1,23 @@
+package routines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Andy Natalino"
+		fmt.Println("Selesai mengirim Data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
