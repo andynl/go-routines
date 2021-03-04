0dd4ce729f054e9c98976d387903d519073a52ed
Andy Natalino <andy.natalino@gmail.com>
2021-03-04 14:03:46 +0700

    Range Channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package routines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -79,4 +80,21 @@ func TestBufferedChannel(t *testing.T) {
 
 	fmt.Println("Selesai")
 
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
 }
